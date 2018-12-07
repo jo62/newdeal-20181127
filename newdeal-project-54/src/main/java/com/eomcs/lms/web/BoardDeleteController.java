6fe896d3833caddde5f182091c1f23d49ef521d0
unknown <jo62>
2018-12-07 18:14:31 +0900

    '1207'

---
+++ newdeal-project-54/src/main/java/com/eomcs/lms/web/BoardDeleteController.java
@@ -0,0 +1,30 @@
+package com.eomcs.lms.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.eomcs.lms.dao.BoardDao;
+
+@Controller
+public class BoardDeleteController {
+
+  BoardDao boardDao;
+
+  public BoardDeleteController(BoardDao boardDao) {
+    this.boardDao = boardDao;
+  }
+  
+  @RequestMapping("/board/delete")
+  public String execute(
+      HttpServletRequest request, HttpServletResponse response)
+          throws Exception {
+
+      int no = Integer.parseInt(request.getParameter("no"));
+      request.setAttribute("count", boardDao.delete(no));
+      response.setContentType("text/html;charset=UTF-8");
+      
+      return "/board/delete.jsp";
+  }
+}
---
+++ newdeal-project-54/src/main/java/com/eomcs/lms/web/LogoutController.java
@@ -0,0 +1,21 @@
+package com.eomcs.lms.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LogoutController {
+  
+  @RequestMapping("/auth/logout")
+  public String logout(
+      HttpServletRequest request, HttpServletResponse response)
+          throws Exception {
+    
+    request.getSession().invalidate();
+    
+    return "redirect:login";
+  }
+}
